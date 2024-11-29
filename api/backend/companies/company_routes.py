fd2e2506a1ba14ef9592aa95f2e131654f8265f0
David Thorpe <davidthorpe007@gmail.com>
2024-11-29 13:12:45 -0500

    Configured necessary files to work with out database + removed unnecessary database files

--- api/backend/companies/company_routes.py
+++ api/backend/companies/company_routes.py
@@ -13,3 +13,16 @@ from backend.db_connection import db
 # Create a new Blueprint object, which is a collection of 
 # routes.
 companies = Blueprint('companies', __name__)
+
+@companies.route('/companies', methods=['GET'])
+def get_customers():
+
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT id, name FROM companies
+    ''')
+    
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
