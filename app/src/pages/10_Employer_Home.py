c76549331f5ea73ca50c0b73c5c9df92d943791e
Chetan-Gabu <gabu.c@northeastern.edu>
2024-12-02 04:20:39 -0500

    Update employer buttons

--- app/src/pages/10_Employer_Home.py
+++ app/src/pages/10_Employer_Home.py
@@ -14,17 +14,17 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('Predict Value Based on Regression Model', 
+if st.button('View Job Openings', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/11_Prediction.py')
 
-if st.button('View the Simple API Demo', 
+if st.button('Search Students', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/12_API_Test.py')
 
-if st.button("View Classification Demo",
+if st.button("View Applications",
              type='primary',
              use_container_width=True):
   st.switch_page('pages/13_Classification.py')
