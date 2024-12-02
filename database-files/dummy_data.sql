0b5d159fd4536f17e514ef0597f5a3c3318fb3b7
ivann <ivanng2121@gmail.com>
2024-12-01 20:21:31 -0500

    password for first user

--- database-files/dummy_data.sql
+++ database-files/dummy_data.sql
@@ -11,4 +11,4 @@ VALUES
 -- Insert into users
 INSERT INTO users (name, firstName, middleName, lastName, mobile, email, passwordHash, profile, companyId, lastLogin)
 VALUES
-    ('John Doe', 'John', 'A', 'Doe', '1234567890', 'johndoe@example.com',
+    ('John Doe', 'John', 'A', 'Doe', '1234567890', 'johndoe@example.com', MD5('password123'), 
