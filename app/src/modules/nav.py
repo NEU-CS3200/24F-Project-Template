31bc65b8ac9eb99132059b5dc6d9f6ef6a9f8b97
Chetan-Gabu <gabu.c@northeastern.edu>
2024-12-02 02:16:22 -0500

    created student sidelinks

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -94,11 +94,12 @@ def SideBarLinks(show_home=False):
             ApiTestNav()
             ClassificationNav()
 
+        # If the user role is student, show student home
+        if st.session_state["role"] == "student":
+            StudentAdvHomeNav()
+
         # If the user is an administrator, give them access to the administrator pages
         if st.session_state["role"] == "administrator":
-            AdminPageNav()
-
-        if st.session_state["role"] == "student":
             AdminPageNav()
 
     # Always show the About page at the bottom of the list of links
