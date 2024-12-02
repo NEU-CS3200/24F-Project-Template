53b58d47485d3047e42087e894fa0a70ca651e4a
ebrune1014 <173849416+ebrune1014@users.noreply.github.com>
2024-12-01 20:53:25 -0500

    Update 02_recruiter_home.py

---
+++ app/src/pages/02_recruiter_home.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from modules.nav import SideBarLinks
+
+import logging
+logger = logging.getLogger(__name__)
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Welcome, {st.session_state['first_name']}!. Let's get prepared.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
+
+if st.button('View Student Reviews', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_student_reviews.py')
+
+if st.button('View Company Reviews (Or add your own!)', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/00_company_reviews.py')
+
+if st.button('View and Filter Most Popular Companies', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/00_popular_companies.py')
