7f4b2bf7f85b982e7e82937d654d4e9ca7a5ac16
christinejahn <ahn.c@northeastern.edu>
2024-12-01 22:12:17 -0500

    updating to input view professional events

--- app/src/pages/30_Student_Sarah_Home.py
+++ app/src/pages/30_Student_Sarah_Home.py
@@ -15,3 +15,8 @@ if st.button('View Advisor Feedback',
              type='primary',
              use_container_width=True):
   st.switch_page('pages/21_Advisor_Feedback.py')
+
+if st.button('View Professional Events', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/31_Professional_Events.py')
