fa5631327e0f83b7c61e487257da965074e8608a
EhlOps <samtehlers@gmail.com>
2024-12-02 14:14:36 -0500

    prettify

--- api/backend/_customers/customer_routes.py
+++ api/backend/_customers/customer_routes.py
@@ -7,7 +7,7 @@ from flask import request
 from flask import jsonify
 from flask import make_response
 from flask import current_app
-from backend._db_connection import db
+from backend.db_connection import db
 from backend._ml_models.model01 import predict
 
 #------------------------------------------------------------
