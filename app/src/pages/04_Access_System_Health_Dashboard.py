2d63ca57c6df306c2741e92054795ed3de61d763
christinejahn <ahn.c@northeastern.edu>
2024-12-02 18:12:33 -0500

    renaming

--- app/src/pages/04_Access_System_Health_Dashboard.py
+++ app/src/pages/04_Access_System_Health_Dashboard.py
@@ -8,7 +8,7 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('Prediction with Regression')
+st.title('System Health Dasbhoard')
 
 # create a 2 column layout
 col1, col2 = st.columns(2)
