77c5dd00db9b80d012988058432301ebf33c60f7
Chetan-Gabu <gabu.c@northeastern.edu>
2024-12-02 13:43:29 -0500

    delete unnesecary tempelate pages

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -63,8 +63,8 @@ def EmployerSearchNav():
         "pages/33_Employer_Search.py", label="Employers", icon="🕴️"
     )
 
-    def ApplicationEditorNav():
-    st.sidebar.page_link(
+def ApplicationEditorNav():
+    st.sidebar.page_link( 
         "pages/34_Application_Editor.py", label="Applications", icon="📝"
     )
 
