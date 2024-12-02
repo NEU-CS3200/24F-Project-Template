3888cc6848013131885f79d8678ade77628e47f8
mmikanguyen <mika.nguyen@gmail.com>
2024-12-01 19:05:14 -0500

    Update nav.py

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -49,11 +49,14 @@ def ClassificationNav():
 
 
 #### ------------------------ Student Housing/Carpool Role ------------------------
-def AdminPageNav():
-    st.sidebar.page_link("pages/20_Student_Kevin_Home.py", label="Student - Kevin Chen", icon="🖥️")
+def FeedbackNav():
+    st.sidebar.page_link("pages/20_Student_Kevin_Home.py", label="Advisor Feedeback", icon="🖥️")
     st.sidebar.page_link(
-        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
+        "pages/21_Advisor_Feedback.py", label="ML Model Management", icon="🏢"
     )
+
+#def HousingNav():
+   # st.sidebar.page_link("pages/20_Student_Kevin_Home.py", label =)
 
 
 # --------------------------------Links Function -----------------------------------------------
@@ -91,7 +94,7 @@ def SideBarLinks(show_home=False):
 
         # If the user is an administrator, give them access to the administrator pages
         if st.session_state["role"] == "Student":
-            AdminPageNav()
+            StudentPageNav()
 
     # Always show the About page at the bottom of the list of links
     AboutPageNav()
