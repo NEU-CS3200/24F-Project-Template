4f235b15eebb532a3ce0f8df8d2364f7becb8285
EhlOps <samtehlers@gmail.com>
2024-12-02 14:16:06 -0500

    employers

---
+++ api/backend/employers/employer_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+from backend._ml_models.model01 import predict
+
+students = Blueprint("employers", __name__)
