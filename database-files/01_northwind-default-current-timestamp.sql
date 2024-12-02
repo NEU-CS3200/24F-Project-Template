ae1e8e9355e4c59827003b0ecd28fc2ceb3e546b
Mehr Anand <mehr.s.anand@gmail.com>
2024-12-02 15:33:46 -0500

    made primary and FK for work_experience

--- database-files/01_northwind-default-current-timestamp.sql
+++ database-files/01_northwind-default-current-timestamp.sql
@@ -119,6 +119,8 @@ CREATE TABLE work_experience
   applicationId int,
   name varchar(30),
   summary text,
-)
+  PRIMARY KEY (applicationId, name),
+  CONSTRAINT fk_10 FOREIGN KEY (applicationId) REFERENCES applications (id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
 
 
