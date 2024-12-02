383c6c3952af64cc26859adaeead1b051ed8bfd0
mmikanguyen <mika.nguyen@gmail.com>
2024-12-01 21:34:17 -0500

    Update community_routes.py

--- api/backend/community/community_routes.py
+++ api/backend/community/community_routes.py
@@ -109,4 +109,39 @@ def update_profile():
     db.get_db().commit()
     return 'profile updated!'
 
+@products.route('/product', methods=['POST'])
+def add_new_product():
+    
+    # In a POST request, there is a 
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    name = the_data['product_name']
+    description = the_data['product_description']
+    price = the_data['product_price']
+    category = the_data['product_category']
+    
+    #INSERT statement
+    query = f'''
+        INSERT INTO products (product_name,
+                              description,
+                              category, 
+                              list_price)
+        VALUES ('{name}', '{description}', '{category}', {str(price)})
+    '''
+ 
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    response = make_response("Successfully added product")
+    response.status_code = 200
+    return response
+
+
 
