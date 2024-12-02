fde1dd1eabee9d67bac577d2f7c7b2aaabaf1ae8
ivann <ivanng2121@gmail.com>
2024-12-01 20:40:45 -0500

    name for fourth user

--- database-files/dummy_data.sql
+++ database-files/dummy_data.sql
@@ -13,4 +13,5 @@ INSERT INTO users (name, firstName, middleName, lastName, mobile, email, passwor
 VALUES
     ('John Doe', 'John', 'A', 'Doe', '1234567890', 'johndoe@example.com', MD5('password123'), 'Senior Engineer at TechCorp', 1, NOW()),
     ('Jane Smith', 'Jane', 'B', 'Smith', '1234567891', 'janesmith@example.com', MD5('securepass'), 'Manager at Innovate Inc', 2, NOW()),
-    ('Samuel Green', 'Samuel', 'C', 'Green', '1234567892', 'samuelgreen@example.com', MD5('mypassword'), 'Data Scientist at Healthify Solutions', 3, NOW())
+    ('Samuel Green', 'Samuel', 'C', 'Green', '1234567892', 'samuelgreen@example.com', MD5('mypassword'), 'Data Scientist at Healthify Solutions', 3, NOW()),
+    ('Emily White', )
