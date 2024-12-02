4790847dc2d23f8566d2c45a1adc3dc0ae68afb0
christinejahn <ahn.c@northeastern.edu>
2024-12-01 22:22:18 -0500

    updating sarah's home page

--- app/src/pages/30_Student_Sarah_Home.py
+++ app/src/pages/30_Student_Sarah_Home.py
@@ -9,7 +9,10 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('Student Home Page')
+st.title(f"Welcome Student, {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
 
 if st.button('View Advisor Feedback', 
              type='primary',
