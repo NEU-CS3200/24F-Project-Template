f56b5726fc97803aece5476415e7b1e98b31a7d0
Harrison Dolgoff <harrisoncdolgoff@gmail.com>
2024-12-03 18:30:55 -0500

    Added first blueprint for applications, importing proper flask modules

---
+++ api/backend/applications/applications.py
@@ -0,0 +1,14 @@
+### FILE FOR APPLICATIONS FROM THE REST API MATRIX
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+
+
+#------------------------------------------------------------
+# Create a new Blueprint object, which is a collection of 
+# routes.
+applications = Blueprint('applications', __name__)
