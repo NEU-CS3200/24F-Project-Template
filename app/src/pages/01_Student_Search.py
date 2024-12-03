d4175f80b18d6fa95549fb456bcff19a4159b724
Chetan-Gabu <gabu.c@northeastern.edu>
2024-12-03 14:42:28 -0500

    search

--- app/src/pages/01_Student_Search.py
+++ app/src/pages/01_Student_Search.py
@@ -7,3 +7,12 @@ from modules.nav import SideBarLinks
 st.set_page_config(layout = 'wide')
 
 SideBarLinks()
+
+st.title(f"Student Search")
+
+text_input = st.text_input(
+    "Search",
+    placeholder="Enter student name or Id#",
+    key="search"
+)
+  
