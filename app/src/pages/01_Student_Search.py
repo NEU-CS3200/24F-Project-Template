9087c884dc00743547740392e9d528fd58e3a422
Chetan-Gabu <gabu.c@northeastern.edu>
2024-12-03 15:33:19 -0500

    student search

--- app/src/pages/01_Student_Search.py
+++ app/src/pages/01_Student_Search.py
@@ -2,6 +2,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 import streamlit as st
+import pandas as pd
+import numpy as np
 from modules.nav import SideBarLinks
 
 st.set_page_config(layout = 'wide')
@@ -9,10 +11,38 @@ st.set_page_config(layout = 'wide')
 SideBarLinks()
 
 st.title(f"Student Search")
+with st.form("student_search"):
+    
+    text_input = st.text_input(
+        "Search",
+        placeholder="Enter student name or Id#",
+        key="search")
 
-text_input = st.text_input(
-    "Search",
-    placeholder="Enter student name or Id#",
-    key="search"
+    if st.session_state["role"] == "coop_advisor":
+        st.radio(
+            "Student Type",
+            key="student_type",
+            options=["All", "Already Advising",],
+        )
+
+    if st.session_state["role"] == "employer":
+        st.radio(
+            "Student Type",
+            key="student_type",
+            options=["All", "Applied", "Flagged",],
+        )
+
+    st.form_submit_button('Search')
+
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame(
+        np.random.randn(12, 5), columns=["a", "b", "c", "d", "e"]
+    )
+
+event = st.dataframe(
+    st.session_state.df,
+    key="data",
+    on_select="rerun",
+    selection_mode=["multi-row", "multi-column"],
 )
-  
+
