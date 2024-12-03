e04773590ab3ce50054af526b423ea1a9f553a3a
Harrison Dolgoff <harrisoncdolgoff@gmail.com>
2024-12-03 18:36:06 -0500

    Created first applications route, given job id and SQL query still needs to be done

--- api/backend/applications/applications.py
+++ api/backend/applications/applications.py
@@ -12,3 +12,18 @@ from backend.db_connection import db
 # Create a new Blueprint object, which is a collection of 
 # routes.
 applications = Blueprint('applications', __name__)
+
+# Making a request given the blueprint
+# Looking at job applications given a job id
+@applications.route('/applications/<int:job_id>', methods=['GET'])
+def job_apps(jobID):
+    cursor = db.get_db().cursor()
+    cursor.execute('''
+    TO BE DONE
+    ''')
+    
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
