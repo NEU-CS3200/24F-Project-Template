624953dbedf431cf662ecf24735b3ec95c919ec6
Chetan-Gabu <gabu.c@northeastern.edu>
2024-12-03 16:54:54 -0500

    rough profile

--- app/src/pages/03_User_Profile.py
+++ app/src/pages/03_User_Profile.py
@@ -7,3 +7,11 @@ from modules.nav import SideBarLinks
 st.set_page_config(layout = 'wide')
 
 st.title(" Full Name's Profile")
+st.image("assets/profile_pic.png", width=150)
+st.write("Pronouns: ")
+st.write("Preferred Name: ")
+st.write("Age:",)
+st.write("Email:",)
+st.write("Phone Number:",)
+st.wrire("Last Active: ")
+st.write("Bio:",)
