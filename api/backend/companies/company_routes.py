886ea84e4daa587968b0934d67614c63eb6ae749
David Thorpe <davidthorpe007@gmail.com>
2024-12-03 19:32:12 -0500

    updated advisor page + company routes

--- api/backend/companies/company_routes.py
+++ api/backend/companies/company_routes.py
@@ -79,3 +79,65 @@ WHERE companies.id = %s''', (company_id,))
     the_response.status_code = 200
     return the_response
 
+@companies.route('/companies/<company_id>/count_applicants', methods=['GET'])
+def get_company_count_applicants(company_id):
+
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT 
+    COUNT(DISTINCT ja.studentId) AS AmountOfApplicants
+FROM companies c
+JOIN job_posting jp ON c.id = jp.companyId
+                   JOIN job_application ja ON jp.id = ja.jobId
+WHERE c.id = %s''', (company_id,))
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+@companies.route('/companies/match_description/<key_word>', methods=['GET'])
+def get_companies_match_description(key_word):
+
+    cursor = db.get_db().cursor()
+    cursor.execute('''
+SELECT 
+    c.id AS CompanyId,
+    c.name AS CompanyName,
+                   jp.name AS JobName,
+    jp.description AS Description 
+FROM companies c
+JOIN job_posting jp ON c.id = jp.companyId
+WHERE jp.description LIKE %s OR c.name LIKE %s OR jp.location LIKE %s
+''', (f"%{key_word}%", f"%{key_word}%", f"%{key_word}%"))
+
+    
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+@companies.route('/companies/avg_gpa_past_employee/<company_id>', methods=['GET'])
+def get_companies_avg_gpa_past_employee(company_id):
+
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT ROUND(AVG(s.gpa), 2) AS AverageGPA, cc.firstName, cc.lastName, cc.phone, cc.email   
+                   FROM students s
+JOIN reviews r ON s.id = r.studentId
+JOIN job_posting jp ON r.jobId = jp.id
+JOIN companies c ON jp.companyId = c.id
+                   JOIN company_contact cc ON c.id = cc.companyId
+WHERE c.id = %s
+                   GROUP BY cc.firstName, cc.lastName, cc.phone, cc.email
+                ''', (company_id,))
+    
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+                   
+                   
+
+
