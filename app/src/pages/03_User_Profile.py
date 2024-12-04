96261cdbb5c4b180ad5a318ed64fb62a4cd5028a
Chetan-Gabu <gabu.c@northeastern.edu>
2024-12-04 03:02:26 -0500

    rough user profile get route

--- app/src/pages/03_User_Profile.py
+++ app/src/pages/03_User_Profile.py
@@ -6,6 +6,18 @@ from modules.nav import SideBarLinks
 
 st.set_page_config(layout = 'wide')
 
+try:
+    response1 = requests.get(
+        f"http://api:4000/stu/simple_routes/users/{st.session_state['profile_id']}"
+    )
+    if response.status_code == 200:
+        if len(response.json()) != 0:
+            df = pd.json_normalize(response.json())
+            st.write(df)
+except requests.exceptions.RequestException as e:
+    st.error(f"Error connecting to server: {str(e)}")
+
+
 st.title(" Full Name's Profile")
 st.image("assets/profile_pic.png", width=150)
 st.write("Pronouns: ")
@@ -16,7 +28,7 @@ st.write("Phone Number:",)
 st.wrire("Last Active: ")
 st.write("Bio:",)
 
-if st.session_state["user_id"] == [user_id]:
+if st.session_state["user_id"] == st.session_state["profile_id"]:
     if st.button('Edit Profile', 
              type='primary',
              use_container_width=True):
