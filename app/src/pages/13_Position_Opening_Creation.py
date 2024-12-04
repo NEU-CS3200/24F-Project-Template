b5af831822f26d1a06f16f883a1fee52384e0e16
Ivan Ng <ivanng2121@gmail.com>
2024-12-04 15:25:17 -0500

    user references

--- app/src/pages/13_Position_Opening_Creation.py
+++ app/src/pages/13_Position_Opening_Creation.py
@@ -7,3 +7,10 @@ from modules.nav import SideBarLinks
 st.set_page_config(layout = 'wide')
 
 SideBarLinks()
+
+st.title(f"Create Opening")
+with st.form("opening data"):
+
+    summary_input = st.text_input(
+        
+    )
