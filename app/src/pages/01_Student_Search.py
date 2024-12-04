d84968adcd4a8a988ae2bc9e11870cc8ffc52790
EhlOps <samtehlers@gmail.com>
2024-12-04 00:54:50 -0500

    Sam

--- app/src/pages/01_Student_Search.py
+++ app/src/pages/01_Student_Search.py
@@ -1,4 +1,5 @@
 import logging
+
 logger = logging.getLogger(__name__)
 
 import streamlit as st
@@ -7,51 +8,47 @@ import pandas as pd
 import numpy as np
 from modules.nav import SideBarLinks
 
-st.set_page_config(layout = 'wide')
+st.set_page_config(layout="wide")
 
 SideBarLinks()
 
 st.title(f"Student Search")
 
 try:
-    test_response = requests.get('http://api:4000/stu/students')
-    
-    if test_response.status_code == 200:
-        student_search_data = test_response.json()
-        category_options = [""] + [category['value'] for category in categories_data]
-    else:
+    test_response = requests.get("http://api:4000/stu/students")
+
+    if not test_response.status_code == 200:
         st.error("Failed to fetch categories")
-        category_options = []
 except requests.exceptions.RequestException as e:
     st.error(f"Error connecting to students API: {str(e)}")
-    category_options = []
 
 with st.form("student_search"):
-    
     student_value = student_input = st.text_input(
         "Search Students",
-        placeholder="Enter student name or Id#",)
+        placeholder="Enter student name or Id#",
+    )
 
-#    if st.session_state["role"] == "employer":
-#        student_type = st.radio(
-#            "Student Type",
-#            options=["All", "Applied", "Flagged",],
-#        )
+    #    if st.session_state["role"] == "employer":
+    #        student_type = st.radio(
+    #            "Student Type",
+    #            options=["All", "Applied", "Flagged",],
+    #        )
 
-    submit_button = st.form_submit_button('Search')
-    
+    submit_button = st.form_submit_button("Search")
+
     if submit_button:
         if not student_value:
             st.error("Please enter a student name or id")
         else:
-            logger.info(f"Product form submitted with data: {student_value}")
-            
+            logger.info(f"Student form submitted with data: {student_value}")
+
             try:
-                response = requests.post(f'http://api:4000/stu/student_search/{student_value}')
+                response = requests.get(
+                    f"http://api:4000/stu/students/search/{student_value}"
+                )
                 if response.status_code == 200:
-                    st.success("Student added successfully!")
+                    st.success(f"{response.json()}")
                 else:
-                    st.error(f"Error adding product: {response.text}")
+                    st.error(f"Error adding accessing api: {response.status_code}")
             except requests.exceptions.RequestException as e:
                 st.error(f"Error connecting to server: {str(e)}")
-
