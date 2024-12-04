ce90c141cb4dff01b56098cd1ce54959f5118647
Kimchihyo <ivanng2121@gmail.com>
2024-12-03 19:58:18 -0500

    coop representative get route

--- api/backend/admins/admin_routes.py
+++ api/backend/admins/admin_routes.py
@@ -10,3 +10,18 @@ from flask import (
 from backend.db_connection import db
 
 admins = Blueprint("admins", __name__)
+
+@admins.route("/coop_rep", methods=["GET"])
+def get_reps():
+    query = """
+        SELECT u.firstName, u.middleName, u.lastName, u.mobile, u.email FROM cosint.users u
+        WHERE u.firstName = 'John' AND u.lastName = 'Doe' AND u.companyId = 1;
+    """
+
+    cursor = db.get_db().cursor()
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
