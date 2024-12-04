72fd1f6d494b1570778863428a90fd40ba7dace1
EhlOps <samtehlers@gmail.com>
2024-12-04 15:23:36 -0500

    complete student search

--- app/src/pages/01_Student_Search.py
+++ app/src/pages/01_Student_Search.py
@@ -36,6 +36,8 @@ with st.form("student_search"):
 
     submit_button = st.form_submit_button("Search")
 
+    df = None
+
     if submit_button:
         if not student_value:
             st.error("Please enter a student name or id")
@@ -52,10 +54,21 @@ with st.form("student_search"):
                 if response1.status_code == 200:
                     if len(response1.json()) != 0:
                         df = pd.json_normalize(response1.json())
-                        st.write(df)
                 if response2.status_code == 200:
                     if len(response2.json()) != 0:
                         df = pd.json_normalize(response2.json())
-                        st.write(df)
             except requests.exceptions.RequestException as e:
                 st.error(f"Error connecting to server: {str(e)}")
+
+if df is not None:
+    for index, row in df.iterrows():
+        with st.expander(f"{row['firstName']} {row['lastName']}"):
+            col1, col2, col3, col4 = st.columns(4)
+            col1.write("##### Name:")
+            col1.write(f"{row['name']}")
+            col2.write("##### Major:")
+            col2.write(f"{row['major']}")
+            col3.write("##### Year:")
+            col3.write(f"{row['year']}")
+            col4.write("##### Contact:")
+            col4.write(f"{row['email']} | {row['mobile']}")
