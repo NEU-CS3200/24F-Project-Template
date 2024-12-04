7910c21162fc0ba46f990d6811a345d756c42d0e
Kimchihyo <ivanng2121@gmail.com>
2024-12-04 02:51:21 -0500

    user personas and their companies

--- database-files/dummy_data.sql
+++ database-files/dummy_data.sql
@@ -6,7 +6,9 @@ VALUES
     ('TechCorp'),
     ('Innovate Inc'),
     ('Healthify Solutions'),
-    ('EduVenture');
+    ('EduVenture'),
+    ('Cosint'),
+    ('Greip');
 
 -- Insert into users
 INSERT INTO users (name, firstName, middleName, lastName, mobile, email, passwordHash, profile, companyId, lastLogin, preferredName, pronouns, major, year, birthday, profilePic, role)
@@ -14,7 +16,11 @@ VALUES
     ('John Doe', 'John', 'A', 'Doe', '1234567890', 'johndoe@example.com', MD5('password123'), 'Senior Engineer at TechCorp', 1, NOW(), 'Jack', 'He/Him', 'Computer Science', 'Senior', '1990-01-01', 'https://example.com/johndoe.jpg', 'Student'),
     ('Jane Smith', 'Jane', 'B', 'Smith', '1234567891', 'janesmith@example.com', MD5('securepass'), 'Manager at Innovate Inc', 2, NOW(), 'Jenny', 'She/Her', 'Business Administration', 'Junior', '1991-02-02', 'https://example.com/janesmith.jpg', 'Admin'),
     ('Samuel Green', 'Samuel', 'C', 'Green', '1234567892', 'samuelgreen@example.com', MD5('mypassword'), 'Data Scientist at Healthify Solutions', 3, NOW(), 'Sam', 'He/They', 'Data Science', 'Senior', '1992-03-03', 'https://example.com/samuelgreen.jpg', 'Advisor'),
-    ('Emily White', 'Emily', 'D', 'White', '1234567893', 'emilywhite@example.com', MD5('whitepass'), 'CTO at EduVenture', 4, NOW(), 'Em', 'She/Her', 'Computer Engineering', 'Senior', '1993-04-04', 'https://example/emilywhite.jpg', 'Employer');
+    ('Emily White', 'Emily', 'D', 'White', '1234567893', 'emilywhite@example.com', MD5('whitepass'), 'CTO at EduVenture', 4, NOW(), 'Em', 'She/Her', 'Computer Engineering', 'Senior', '1993-04-04', 'https://example/emilywhite.jpg', 'Employer'),
+    ('Mark Fontenot', 'Mark', 'N', 'Fontenot', '1234567894', 'markfont@example.com', MD5('database'), 'Junior at NEU', NULL, NOW(), 'Mark', 'He/Him', 'Computer Science', 'Junior', '2000-05-01', 'https://example/markf.jpg', 'Student'),
+    ('Jordan Michael', 'Jordan', 'O', 'Michael', '1234567895', 'joming@example.com', MD5('basketball'), 'Admin at Consint', 5, NOW(), 'Jordan', 'He/Him', NULL, NULL, '1989-01-01', 'https://example.com/mj.jpg', 'Admin'),
+    ('Joe Montana', 'Joe', 'P', 'Montana', '1234567896', 'jmontana@example.com', MD5('hannah'), 'Advisor at NEU', NULL, NOW(), 'Joe', 'He/Him', NULL, NULL, '1985-02-28', 'https://example.com/hmontana.jpg', 'Advisor'),
+    ('Peter Burke', 'Peter', 'S', 'Burke', '1234567897', 'peterb@example.com', MD5('whitecollar'), 'CEO at Greip', 6, NOW(), 'Pete', 'He/Him', NULL, NULL, '1987-05-11', 'https://example.com/peterb.jpg', 'Employer');
 
 -- Insert into user_references
 INSERT INTO user_references (name, firstName, middleName, lastName, mobile, email, referral, userId) 
