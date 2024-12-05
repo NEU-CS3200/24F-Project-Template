199cc7847ad862fefdbf54def4e63a283cdec43f
Chetan-Gabu <gabu.c@northeastern.edu>
2024-12-04 20:42:05 -0500

    temp create ticket
    minor bugs

--- app/src/pages/22_View_Tickets.py
+++ app/src/pages/22_View_Tickets.py
@@ -11,7 +11,6 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 st.title(f"Tickets")
-
 try:
     test_response = requests.get("http://api:4000/adm/tickets")
 
@@ -52,4 +51,27 @@ with st.form("ticket_search"):
                         st.write(df)
             except requests.exceptions.RequestException as e:
                 st.error(f"Error connecting to server: {str(e)}")
+
+try:
+    response = requests.get(
+        "http://api:4000/adm/tickets"
+    )
+    if response.status_code == 200:
+        if len(response1.json()) != 0:
+            df = pd.json_normalize(response1.json())
+except requests.exceptions.RequestException as e:
+    st.error(f"Error connecting to server: {str(e)}")
+
+if df is not None:
+    for index, row in df.iterrows():
+        with st.expander(f"{row['user']} {row['lastName']}"):
+            col1, col2, col3, col4 = st.columns(4)
+            col1.write("##### Name:")
+            col1.write(f"{row['name']}")
+            col2.write("##### Major:")
+            col2.write(f"{row['major']}")
+            col3.write("##### Year:")
+            col3.write(f"{row['year']}")
+            col4.write("##### Contact:")
+            col4.write(f"{row['email']} | {row['mobile']}")
     
