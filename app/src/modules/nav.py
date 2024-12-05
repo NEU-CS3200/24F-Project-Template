23d8ba2642eda18b8d7c89aa53dc0dfba13d7e07
mmikanguyen <mika.nguyen@gmail.com>
2024-12-05 00:03:45 -0500

    Update nav.py

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -32,7 +32,7 @@ def JessicaPageNav():
 def KevinPageNav():
     st.sidebar.page_link("pages/20_Student_Kevin_Home.py", label="Student Home", icon="📖")
     st.sidebar.page_link("pages/23_My_Profile.py", label="My Profile", icon="👤")
-    st.sidebar.page_link("pages/22_Housing_Carpool.py", label="Housing & Transit", icon="🏘️")
+    st.sidebar.page_link("pages/22_Housing_Carpool.py", label="Housing & Transit", icon="🔍")
     st.sidebar.page_link("pages/21_Advisor_Rec.py", label="Advisor Communications", icon="🏫")
     
 def SarahPageNav():
